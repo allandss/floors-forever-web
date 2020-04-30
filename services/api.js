24115142f34f141bd80f363f41fa82d0143cc29e
Allan Santos <allandssilva@gmail.com>
2020-04-30 04:47:05 -0300

    up

--- services/api.js
+++ services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  //baseURL: 'https://floors-forever-api.herokuapp.com/',
-  baseURL: 'http://localhost:3333'
+  baseURL: 'https://floors-forever-api.herokuapp.com/',
+  //baseURL: 'http://localhost:3333'
 });
 
 export default api;
